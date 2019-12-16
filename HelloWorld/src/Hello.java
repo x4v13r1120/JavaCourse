9bed9b7881648c6a9939f61f5f4f27c6e7a490cf
xavier <xaviernaranjo1130@gmail.com>
2019-12-16 15:03:18 -0500

    Initial commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Xavier");
+
+        int myFirstNumber = (10 * 2)+ (6 * 3);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6+9;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastNumber = 1000- myTotal;
+        System.out.println(myLastNumber);
+    }
+}
