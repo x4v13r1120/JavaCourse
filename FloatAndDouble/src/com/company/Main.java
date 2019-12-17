44f05b3158df84b51c0aa12a971b212711dc67f0
xavier <xaviernaranjo1130@gmail.com>
2019-12-16 21:49:33 -0500

    challenge for double and float was ot convert pounds to kilograms

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -13,12 +13,23 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value= " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 /3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00d / 3.00d;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoubleValue= " + myDoubleValue);
 
 
+        double numberOfPounds = 200d;
+        double numberOfKilograms = numberOfPounds / 0.45259237d;
+        System.out.println("Number of Kilograms = " + numberOfKilograms);
 
-        
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
+
     }
 }
