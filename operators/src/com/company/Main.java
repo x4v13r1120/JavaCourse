3db3726f257f2bb21da2de12dc8169ac5aa32d48
xavier <xaviernaranjo1130@gmail.com>
2019-12-20 14:08:03 -0500

    shortcuts

--- operators/src/com/company/Main.java
+++ operators/src/com/company/Main.java
@@ -66,5 +66,14 @@ public class Main {
             System.out.println("Either or both of the conditions are true");
         }
 
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if (!isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
     }
 }
