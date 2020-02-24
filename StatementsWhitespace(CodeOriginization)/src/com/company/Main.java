fa0d1d3d74f7bf38d0557b9058c4090512e17b75
xavier <xaviernaranjo1130@gmail.com>
2020-02-24 14:33:28 -0500

    adding another video

---
+++ StatementsWhitespace(CodeOriginization)/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+//to reformat code go to code and click reformat for mac is command option L
+    public static void main(String[] args) {
+	int myVariable = 50;
+
+	if(myVariable == 50){
+        System.out.println("Printed");
+    }
+
+
+	myVariable++;
+	myVariable--;
+	System.out.println("This is a test");
+
+	System.out.println("this is" +
+            "another" +
+            "still more");
+	int anotherVariable = 40;anotherVariable--;System.out.println("This is another one");
+    }
+}
